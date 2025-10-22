a02720ec959553340d0ad1c47313ea4bf8954ad0
James937589 <805876@pdsb.net>
2025-10-22 19:17:06 -0400

    hi

---
+++ api/main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI
+
+# CORS (Cross-Origin Resource Sharing)
+# allows us to restrict/enable which
+# client urls are allowed to call 
+# this backend code. 
+# CORS is part of the FastAPI library.
+from fastapi.middleware.cors import CORSMiddleware
+
+# Initialize the FastAPI application
+app = FastAPI(
+    title="FastAPI Example",
+    description="This is an example of using FastAPI"
+)
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # star means all client urls allowed 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+data= [
+    {"name": "alice","age": 30, "city": "New york"},
+ 
+]
+
+
+
+
+
+
+@app.get("/")           #endpoint, or route, always starts with a forward slash
+def default_route():    #route handler function
+    """
+    This is the default endpoint for this back-end.
+    """
+    return "You have reached the default route. Back-end server is listening..."
+    
+@app.get("/list")  
+def get_list():    
+    """
+    This endpoint returns a list of JSON objects.
+    """
+    return {"message": "Greetings! Brought to you by The Back-End Example Endpoint!"}
+
+
+# TO RUN:
+# 1. Put this code in api/main.py and deploy to Vercel
+# 2. Test by using your-vercel-backend-url/docs
+# 3. Later call from front-end using JavaScript fetch()
